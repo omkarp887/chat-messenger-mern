90ea97456f7f714b377c1d027287cd78229ff851
Omkar Patil <omkarp666@gmail.com>
2023-03-13 19:26:31 +0530

    addded api path

--- frontend/src/store/actions/authAction.js
+++ frontend/src/store/actions/authAction.js
@@ -65,7 +65,7 @@ export const userLogin = (data) => {
 
 export const userLogout = () => async(dispatch) => {
      try{
-         const response = await axios.post('/api/messenger/user-logout');
+         const response = await axios.post('https://chat-messenger-mern-sever.vercel.app/api/messenger/user-logout');
          if(response.data.success){
              localStorage.removeItem('authToken');
              dispatch({
