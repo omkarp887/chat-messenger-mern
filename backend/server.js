97ca75491a98139ec7d5617debdcc4b35a4a1139
Omkar Patil <omkarp666@gmail.com>
2023-03-13 19:59:50 +0530

    asdd

--- backend/server.js
+++ backend/server.js
@@ -1,32 +1,35 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const dotenv = require('dotenv')
+const dotenv = require("dotenv");
 const path = require("path");
-const databaseConnect = require('./config/database')
-const authRouter = require('./routes/authRoute')
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-const messengerRoute = require('./routes/messengerRoute');
+const databaseConnect = require("./config/database");
+const authRouter = require("./routes/authRoute");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const messengerRoute = require("./routes/messengerRoute");
 
 dotenv.config({
-     path : 'backend/config/config.env'
-})
+  path: "backend/config/config.env",
+});
 
 app.use(bodyParser.json());
 app.use(cookieParser());
-app.use('/api/messenger',authRouter);
-app.use('/api/messenger',messengerRoute);
+app.use("/api/messenger", authRouter);
+app.use("/api/messenger", messengerRoute);
 
-
-const PORT = process.env.PORT || 5000
-app.get('/', (req, res)=>{
-     res.send('This is from backend Sever')
-})
+const PORT = process.env.PORT || 5000;
+app.get("/", (req, res) => {
+  res.send("This is from backend Sever");
+});
 
 databaseConnect();
 
+app.use(express.static(path.join(__dirname, "../frontend/build")));
 
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+});
 
-app.listen(PORT, ()=>{
-     console.log(`Server is running on port ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
