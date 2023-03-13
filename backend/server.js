342e6ee461b9b35f86ba29a138eee233e2adfabc
Omkar Patil <omkarp666@gmail.com>
2023-03-13 19:38:35 +0530

    server.js changr

--- backend/server.js
+++ backend/server.js
@@ -25,11 +25,7 @@ app.get('/', (req, res)=>{
 
 databaseConnect();
 
-app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-app.get("*", (req, res)=>{
-     res.sendFile(path.resolve(__dirname,"../frontend/build/index.html"));
-})
 
 app.listen(PORT, ()=>{
      console.log(`Server is running on port ${PORT}`)
